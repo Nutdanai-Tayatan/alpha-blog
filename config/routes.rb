36164cd2e397f238707bc86a09b3ec277fdb7791
Nutdanai <nud.thy@gmail.com>
2021-11-15 11:41:46 +0700

    root route/homepage/about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'main/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'main#home'
+
+  get 'about', to: 'main#about'
 end
